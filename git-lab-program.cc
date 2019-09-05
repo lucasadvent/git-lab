7c2f8ffec9f8bc205924ce212297b5b93eb58f64
Lucas <la213718@ohio.edu>
2019-09-05 16:58:40 -0400

    git-lablprogram.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+// File: git-lab-program.cc
+// Author: Lucas Advent
+// Date: Sep 5, 2019
+// description: prints a message
+
+
+#include<iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]){
+
+    cout << "hello git!!" << endl;
+    return 0; 
+}
